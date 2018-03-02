5d43489063cc043fb3fabdd68ee4e4dce39e813e
Maxim Kornilov <sing1e7max@gmail.com>
2018-03-02 13:07:52 +0500

    Add commands rewriter for help command

---
+++ src/VoidMain.Application.Commands/Help/HelpCommandsRewriter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using VoidMain.Application.Commands.Model;
+using VoidMain.Application.Commands.Resolving;
+using VoidMain.CommandLineIinterface.Parser;
+using VoidMain.Hosting;
+
+namespace VoidMain.Application.Commands.Help
+{
+    public class HelpCommandsRewriter
+    {
+        private readonly string HelpName = "help";
+
+        private readonly ApplicationModel _appModel;
+        private readonly ICommandResolver _commandResolver;
+        private readonly IEqualityComparer<string> _identifierComparer;
+
+        public HelpCommandsRewriter(
+            ApplicationModel appModel, ICommandResolver commandResolver,
+            CommandLineSyntaxOptions syntaxOptions = null)
+        {
+            _appModel = appModel ?? throw new ArgumentNullException(nameof(appModel));
+            _commandResolver = commandResolver ?? throw new ArgumentNullException(nameof(commandResolver));
+            _identifierComparer = syntaxOptions?.IdentifierComparer
+                ?? CommandLineSyntaxOptions.DefaultIdentifierComparer;
+        }
+
+        public bool TryRewrite(Dictionary<string, object> context)
+        {
+            if (!context.TryGetValue(ContextKey.CommandOptions, out var optionsValue))
+            {
+                return false;
+            }
+
+            var options = (KeyValuePair<string, string>[])optionsValue;
+
+            bool hasHelpOption = options.Any(IsHelpOption);
+            if (!hasHelpOption)
+            {
+                return false;
+            }
+
+            if (!context.TryGetValue(ContextKey.CommandName, out var commandNameValue))
+            {
+                return false;
+            }
+
+            var name = new CommandName((string[])commandNameValue);
+            if (name.Parts.Count == 0)
+            {
+                return false;
+            }
+
+            var command = _commandResolver.Resolve(context, _appModel.Commands);
+            bool hasSelfHelpOption = command.Arguments.Any(IsHelpOption);
+            if (hasSelfHelpOption)
+            {
+                return false;
+            }
+
+            context[ContextKey.CommandName] = new[] { "help" };
+            context[ContextKey.CommandOperands] = new[] { name.ToString() };
+
+            return true;
+        }
+
+        private bool IsHelpOption(KeyValuePair<string, string> option)
+        {
+            return _identifierComparer.Equals(option.Key, HelpName);
+        }
+
+        private bool IsHelpOption(ArgumentModel arg)
+        {
+            return arg.Kind == ArgumentKind.Option
+                && _identifierComparer.Equals(arg.Name, HelpName);
+        }
+    }
+}
---
+++ src/VoidMain.Application.Commands/Help/HelpCommandsRewriterApplicationBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using VoidMain.Application.Builder;
+using VoidMain.Application.Commands.Help;
+
+namespace VoidMain.Application.Commands.Builder
+{
+    public static class HelpCommandsRewriterApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseHelpCommandsRewriter(this IApplicationBuilder app)
+        {
+            var rewriter = ActivatorUtilities.CreateInstance<HelpCommandsRewriter>(app.Services);
+            app.Use(next =>
+            {
+                return async context =>
+                {
+                    rewriter.TryRewrite(context);
+                    await next(context);
+                };
+            });
+            return app;
+        }
+    }
+}
