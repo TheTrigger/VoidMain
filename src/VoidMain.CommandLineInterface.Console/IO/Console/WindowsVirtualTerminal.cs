b73e5b008286ee4c3b64ff76befc816daa5c20c6
Maxim Kornilov <sing1e7max@gmail.com>
2018-11-04 08:27:58 +0500

    Add TryDisable to WindowsVirtualTerminal

--- src/VoidMain.CommandLineInterface.Console/IO/Console/WindowsVirtualTerminal.cs
+++ src/VoidMain.CommandLineInterface.Console/IO/Console/WindowsVirtualTerminal.cs
@@ -5,26 +5,43 @@ namespace VoidMain.CommandLineInterface.IO.Console
 {
     public static class WindowsVirtualTerminal
     {
-        public static bool TryEnable()
+        public static bool TryEnable() => TryChangeMode(true);
+        public static bool TryDisable() => TryChangeMode(false);
+
+        private static bool TryChangeMode(bool enabled)
         {
-            var stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-            if (stdOut == IntPtr.Zero)
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 return false;
             }
 
-            if (!GetConsoleMode(stdOut, out uint outMode))
+            var handle = GetStdHandle(STD_OUTPUT_HANDLE);
+            if (handle == IntPtr.Zero)
             {
                 return false;
             }
 
-            outMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-            return SetConsoleMode(stdOut, outMode);
+            if (!GetConsoleMode(handle, out uint mode))
+            {
+                return false;
+            }
+
+            if (enabled)
+            {
+                mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+            }
+            else
+            {
+                mode &= ~ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+            }
+
+            return SetConsoleMode(handle, mode);
         }
 
         private const int STD_OUTPUT_HANDLE = -11;
         private const uint ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004;
 
+#pragma warning disable PC003 // Native API not available in UWP
         [DllImport("kernel32.dll")]
         private static extern IntPtr GetStdHandle(int stdHandle);
 
@@ -33,5 +50,6 @@ namespace VoidMain.CommandLineInterface.IO.Console
 
         [DllImport("kernel32.dll")]
         private static extern bool SetConsoleMode(IntPtr consoleHandle, uint mode);
+#pragma warning restore PC003 // Native API not available in UWP
     }
 }
