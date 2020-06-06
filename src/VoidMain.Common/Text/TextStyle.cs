a0096584fb32a60b3675b829bb434d7ba0fbe616
Maxim Kornilov <sing1e7max@gmail.com>
2020-06-06 13:18:47 +0500

    Set default background and foreground in ConsoleStyledTextWriter

--- src/VoidMain.Common/Text/TextStyle.cs
+++ src/VoidMain.Common/Text/TextStyle.cs
@@ -2,9 +2,39 @@
 
 namespace VoidMain.Text
 {
-    public struct TextStyle
+    public readonly struct TextStyle : IEquatable<TextStyle>
     {
-        public Nullable<Color> Foreground { get; set; }
-        public Nullable<Color> Background { get; set; }
+        public Nullable<Color> Foreground { get; }
+        public Nullable<Color> Background { get; }
+
+        public TextStyle(
+            Nullable<Color> foreground = default,
+            Nullable<Color> background = default)
+        {
+            Foreground = foreground;
+            Background = background;
+        }
+
+        public static readonly TextStyle Default = default;
+
+        public override string ToString()
+        {
+            string f = Foreground?.ToString() ?? " --- ";
+            string b = Background?.ToString() ?? " --- ";
+            return $"{f}/{b}";
+        }
+
+        public override int GetHashCode()
+            => HashCode.Combine(Foreground, Background);
+
+        public override bool Equals(object? obj)
+            => obj is TextStyle style && Equals(style);
+
+        public bool Equals(TextStyle other)
+            => Foreground == other.Foreground && Background == other.Background;
+
+        public static bool operator ==(in TextStyle a, in TextStyle b) => a.Equals(b);
+
+        public static bool operator !=(in TextStyle a, in TextStyle b) => !a.Equals(b);
     }
 }
--- src/VoidMain.IO.Console/ConsoleStyledTextWriter.cs
+++ src/VoidMain.IO.Console/ConsoleStyledTextWriter.cs
@@ -6,7 +6,9 @@ namespace VoidMain.IO.Console
 {
     public class ConsoleStyledTextWriter : ConsoleTextWriter, IStyledTextWriter<TextStyle>
     {
-        private readonly string _resetColor = "\x1b[0m";
+        private const string _resetStyle = "\x1B[0m";
+        private const string _defaultForeground = "\x1B[39m";
+        private const string _defaultBackground = "\x1B[49m";
         private readonly ReadOnlyMemory<char> _format;
         private readonly Memory<char> _setColor;
         private readonly Memory<char> _redSpan;
@@ -16,22 +18,17 @@ namespace VoidMain.IO.Console
         public ConsoleStyledTextWriter(IConsole console)
             : base(console)
         {
-            _setColor = "\x1b[38;2;000;000;000m".ToCharArray();
+            _setColor = "\x1B[38;2;000;000;000m".ToCharArray();
             _redSpan = _setColor.Slice(7, 3);
             _greenSpan = _setColor.Slice(11, 3);
             _blueSpan = _setColor.Slice(15, 3);
             _format = "D3".AsMemory();
         }
 
-        public void ClearStyle() => _console.Write(_resetColor);
+        public void ClearStyle() => _console.Write(_resetStyle);
 
         public void WriteStyle(TextStyle style)
         {
-            if (style.Background is null || style.Foreground is null)
-            {
-                ClearStyle();
-            }
-
             var span = _setColor.Span;
 
             if (style.Foreground is Color foreground)
@@ -40,11 +37,20 @@ namespace VoidMain.IO.Console
                 FormatColor(foreground);
                 _console.Write(span);
             }
+            else
+            {
+                _console.Write(_defaultForeground);
+            }
+
             if (style.Background is Color background)
             {
                 span[2] = '4'; // 48 is background
                 FormatColor(background);
                 _console.Write(span);
+            }
+            else
+            {
+                _console.Write(_defaultBackground);
             }
         }
 
