bb7b044a495c964f18c32c39e232029327ce3d75
Maxim Kornilov <sing1e7max@gmail.com>
2020-07-17 16:57:37 +0500

    Make ConsoleKeyConverter 8 times faster

--- src/VoidMain.IO.Console/ConsoleKeyConverter.cs
+++ src/VoidMain.IO.Console/ConsoleKeyConverter.cs
@@ -1,172 +1,167 @@
 ﻿using System;
-using System.Collections.Generic;
 using VoidMain.IO.Keyboard;
 
 namespace VoidMain.IO.Console
 {
     public class ConsoleKeyConverter
     {
-        private readonly Dictionary<ConsoleKey, Key> _keys;
+        private readonly Key[] _keys;
+        private readonly KeyModifiers[] _modifiers;
 
         public ConsoleKeyConverter()
         {
-            _keys = new Dictionary<ConsoleKey, Key>
-            {
-                [ConsoleKey.D0] = Key.D0,
-                [ConsoleKey.D1] = Key.D1,
-                [ConsoleKey.D2] = Key.D2,
-                [ConsoleKey.D3] = Key.D3,
-                [ConsoleKey.D4] = Key.D4,
-                [ConsoleKey.D5] = Key.D5,
-                [ConsoleKey.D6] = Key.D6,
-                [ConsoleKey.D7] = Key.D7,
-                [ConsoleKey.D8] = Key.D8,
-                [ConsoleKey.D9] = Key.D9,
-                [ConsoleKey.A] = Key.A,
-                [ConsoleKey.B] = Key.B,
-                [ConsoleKey.C] = Key.C,
-                [ConsoleKey.D] = Key.D,
-                [ConsoleKey.E] = Key.E,
-                [ConsoleKey.F] = Key.F,
-                [ConsoleKey.G] = Key.G,
-                [ConsoleKey.H] = Key.H,
-                [ConsoleKey.I] = Key.I,
-                [ConsoleKey.J] = Key.J,
-                [ConsoleKey.K] = Key.K,
-                [ConsoleKey.L] = Key.L,
-                [ConsoleKey.M] = Key.M,
-                [ConsoleKey.N] = Key.N,
-                [ConsoleKey.O] = Key.O,
-                [ConsoleKey.P] = Key.P,
-                [ConsoleKey.Q] = Key.Q,
-                [ConsoleKey.R] = Key.R,
-                [ConsoleKey.S] = Key.S,
-                [ConsoleKey.T] = Key.T,
-                [ConsoleKey.U] = Key.U,
-                [ConsoleKey.V] = Key.V,
-                [ConsoleKey.W] = Key.W,
-                [ConsoleKey.X] = Key.X,
-                [ConsoleKey.Y] = Key.Y,
-                [ConsoleKey.Z] = Key.Z,
-                [ConsoleKey.NumPad0] = Key.NumPad0,
-                [ConsoleKey.NumPad1] = Key.NumPad1,
-                [ConsoleKey.NumPad2] = Key.NumPad2,
-                [ConsoleKey.NumPad3] = Key.NumPad3,
-                [ConsoleKey.NumPad4] = Key.NumPad4,
-                [ConsoleKey.NumPad5] = Key.NumPad5,
-                [ConsoleKey.NumPad6] = Key.NumPad6,
-                [ConsoleKey.NumPad7] = Key.NumPad7,
-                [ConsoleKey.NumPad8] = Key.NumPad8,
-                [ConsoleKey.NumPad9] = Key.NumPad9,
-                [ConsoleKey.Multiply] = Key.Multiply,
-                [ConsoleKey.Add] = Key.Add,
-                [ConsoleKey.Separator] = Key.Separator,
-                [ConsoleKey.Subtract] = Key.Subtract,
-                [ConsoleKey.Decimal] = Key.Decimal,
-                [ConsoleKey.Divide] = Key.Divide,
-                [ConsoleKey.F1] = Key.F1,
-                [ConsoleKey.F2] = Key.F2,
-                [ConsoleKey.F3] = Key.F3,
-                [ConsoleKey.F4] = Key.F4,
-                [ConsoleKey.F5] = Key.F5,
-                [ConsoleKey.F6] = Key.F6,
-                [ConsoleKey.F7] = Key.F7,
-                [ConsoleKey.F8] = Key.F8,
-                [ConsoleKey.F9] = Key.F9,
-                [ConsoleKey.F10] = Key.F10,
-                [ConsoleKey.F11] = Key.F11,
-                [ConsoleKey.F12] = Key.F12,
-                [ConsoleKey.F13] = Key.F13,
-                [ConsoleKey.F14] = Key.F14,
-                [ConsoleKey.F15] = Key.F15,
-                [ConsoleKey.F16] = Key.F16,
-                [ConsoleKey.F17] = Key.F17,
-                [ConsoleKey.F18] = Key.F18,
-                [ConsoleKey.F19] = Key.F19,
-                [ConsoleKey.F20] = Key.F20,
-                [ConsoleKey.F21] = Key.F21,
-                [ConsoleKey.F22] = Key.F22,
-                [ConsoleKey.F23] = Key.F23,
-                [ConsoleKey.F24] = Key.F24,
-                [ConsoleKey.LeftArrow] = Key.LeftArrow,
-                [ConsoleKey.UpArrow] = Key.UpArrow,
-                [ConsoleKey.RightArrow] = Key.RightArrow,
-                [ConsoleKey.DownArrow] = Key.DownArrow,
-                [ConsoleKey.Insert] = Key.Insert,
-                [ConsoleKey.Delete] = Key.Delete,
-                [ConsoleKey.End] = Key.End,
-                [ConsoleKey.Home] = Key.Home,
-                [ConsoleKey.PageUp] = Key.PageUp,
-                [ConsoleKey.PageDown] = Key.PageDown,
-                [ConsoleKey.Backspace] = Key.Backspace,
-                [ConsoleKey.Tab] = Key.Tab,
-                [ConsoleKey.Spacebar] = Key.Spacebar,
-                [ConsoleKey.Enter] = Key.Enter,
-                [ConsoleKey.Escape] = Key.Escape,
-                [ConsoleKey.Sleep] = Key.Sleep,
-                [ConsoleKey.PrintScreen] = Key.PrintScreen,
-                [ConsoleKey.LeftWindows] = Key.LeftWindows,
-                [ConsoleKey.RightWindows] = Key.RightWindows,
-                [ConsoleKey.Applications] = Key.Applications,
-                [ConsoleKey.MediaNext] = Key.MediaNext,
-                [ConsoleKey.MediaPrevious] = Key.MediaPrevious,
-                [ConsoleKey.MediaStop] = Key.MediaStop,
-                [ConsoleKey.MediaPlay] = Key.MediaPlay,
-                [ConsoleKey.VolumeMute] = Key.VolumeMute,
-                [ConsoleKey.VolumeDown] = Key.VolumeDown,
-                [ConsoleKey.VolumeUp] = Key.VolumeUp,
-                [ConsoleKey.LaunchApp1] = Key.LaunchApp1,
-                [ConsoleKey.LaunchApp2] = Key.LaunchApp2,
-                [ConsoleKey.LaunchMail] = Key.LaunchMail,
-                [ConsoleKey.BrowserBack] = Key.BrowserBack,
-                [ConsoleKey.BrowserForward] = Key.BrowserForward,
-                [ConsoleKey.BrowserRefresh] = Key.BrowserRefresh,
-                [ConsoleKey.BrowserStop] = Key.BrowserStop,
-                [ConsoleKey.BrowserSearch] = Key.BrowserSearch,
-                [ConsoleKey.BrowserFavorites] = Key.BrowserFavorites,
-                [ConsoleKey.BrowserHome] = Key.BrowserHome,
-                [ConsoleKey.Oem1] = Key.OemSemicolon,
-                [ConsoleKey.Oem2] = Key.OemQuestion,
-                [ConsoleKey.Oem3] = Key.OemTilde,
-                [ConsoleKey.Oem4] = Key.OemOpenBrackets,
-                [ConsoleKey.Oem5] = Key.OemPipe,
-                [ConsoleKey.Oem6] = Key.OemCloseBrackets,
-                [ConsoleKey.Oem7] = Key.OemQuotes,
-                [ConsoleKey.Oem102] = Key.OemBackslash,
-                [ConsoleKey.OemPlus] = Key.OemPlus,
-                [ConsoleKey.OemComma] = Key.OemComma,
-                [ConsoleKey.OemMinus] = Key.OemMinus,
-                [ConsoleKey.OemPeriod] = Key.OemPeriod
-            };
+            _keys = new Key[256];
+            Array.Fill(_keys, Key.Unknown);
+
+            _keys[(int)ConsoleKey.D0] = Key.D0;
+            _keys[(int)ConsoleKey.D1] = Key.D1;
+            _keys[(int)ConsoleKey.D2] = Key.D2;
+            _keys[(int)ConsoleKey.D3] = Key.D3;
+            _keys[(int)ConsoleKey.D4] = Key.D4;
+            _keys[(int)ConsoleKey.D5] = Key.D5;
+            _keys[(int)ConsoleKey.D6] = Key.D6;
+            _keys[(int)ConsoleKey.D7] = Key.D7;
+            _keys[(int)ConsoleKey.D8] = Key.D8;
+            _keys[(int)ConsoleKey.D9] = Key.D9;
+            _keys[(int)ConsoleKey.A] = Key.A;
+            _keys[(int)ConsoleKey.B] = Key.B;
+            _keys[(int)ConsoleKey.C] = Key.C;
+            _keys[(int)ConsoleKey.D] = Key.D;
+            _keys[(int)ConsoleKey.E] = Key.E;
+            _keys[(int)ConsoleKey.F] = Key.F;
+            _keys[(int)ConsoleKey.G] = Key.G;
+            _keys[(int)ConsoleKey.H] = Key.H;
+            _keys[(int)ConsoleKey.I] = Key.I;
+            _keys[(int)ConsoleKey.J] = Key.J;
+            _keys[(int)ConsoleKey.K] = Key.K;
+            _keys[(int)ConsoleKey.L] = Key.L;
+            _keys[(int)ConsoleKey.M] = Key.M;
+            _keys[(int)ConsoleKey.N] = Key.N;
+            _keys[(int)ConsoleKey.O] = Key.O;
+            _keys[(int)ConsoleKey.P] = Key.P;
+            _keys[(int)ConsoleKey.Q] = Key.Q;
+            _keys[(int)ConsoleKey.R] = Key.R;
+            _keys[(int)ConsoleKey.S] = Key.S;
+            _keys[(int)ConsoleKey.T] = Key.T;
+            _keys[(int)ConsoleKey.U] = Key.U;
+            _keys[(int)ConsoleKey.V] = Key.V;
+            _keys[(int)ConsoleKey.W] = Key.W;
+            _keys[(int)ConsoleKey.X] = Key.X;
+            _keys[(int)ConsoleKey.Y] = Key.Y;
+            _keys[(int)ConsoleKey.Z] = Key.Z;
+            _keys[(int)ConsoleKey.NumPad0] = Key.NumPad0;
+            _keys[(int)ConsoleKey.NumPad1] = Key.NumPad1;
+            _keys[(int)ConsoleKey.NumPad2] = Key.NumPad2;
+            _keys[(int)ConsoleKey.NumPad3] = Key.NumPad3;
+            _keys[(int)ConsoleKey.NumPad4] = Key.NumPad4;
+            _keys[(int)ConsoleKey.NumPad5] = Key.NumPad5;
+            _keys[(int)ConsoleKey.NumPad6] = Key.NumPad6;
+            _keys[(int)ConsoleKey.NumPad7] = Key.NumPad7;
+            _keys[(int)ConsoleKey.NumPad8] = Key.NumPad8;
+            _keys[(int)ConsoleKey.NumPad9] = Key.NumPad9;
+            _keys[(int)ConsoleKey.Multiply] = Key.Multiply;
+            _keys[(int)ConsoleKey.Add] = Key.Add;
+            _keys[(int)ConsoleKey.Separator] = Key.Separator;
+            _keys[(int)ConsoleKey.Subtract] = Key.Subtract;
+            _keys[(int)ConsoleKey.Decimal] = Key.Decimal;
+            _keys[(int)ConsoleKey.Divide] = Key.Divide;
+            _keys[(int)ConsoleKey.F1] = Key.F1;
+            _keys[(int)ConsoleKey.F2] = Key.F2;
+            _keys[(int)ConsoleKey.F3] = Key.F3;
+            _keys[(int)ConsoleKey.F4] = Key.F4;
+            _keys[(int)ConsoleKey.F5] = Key.F5;
+            _keys[(int)ConsoleKey.F6] = Key.F6;
+            _keys[(int)ConsoleKey.F7] = Key.F7;
+            _keys[(int)ConsoleKey.F8] = Key.F8;
+            _keys[(int)ConsoleKey.F9] = Key.F9;
+            _keys[(int)ConsoleKey.F10] = Key.F10;
+            _keys[(int)ConsoleKey.F11] = Key.F11;
+            _keys[(int)ConsoleKey.F12] = Key.F12;
+            _keys[(int)ConsoleKey.F13] = Key.F13;
+            _keys[(int)ConsoleKey.F14] = Key.F14;
+            _keys[(int)ConsoleKey.F15] = Key.F15;
+            _keys[(int)ConsoleKey.F16] = Key.F16;
+            _keys[(int)ConsoleKey.F17] = Key.F17;
+            _keys[(int)ConsoleKey.F18] = Key.F18;
+            _keys[(int)ConsoleKey.F19] = Key.F19;
+            _keys[(int)ConsoleKey.F20] = Key.F20;
+            _keys[(int)ConsoleKey.F21] = Key.F21;
+            _keys[(int)ConsoleKey.F22] = Key.F22;
+            _keys[(int)ConsoleKey.F23] = Key.F23;
+            _keys[(int)ConsoleKey.F24] = Key.F24;
+            _keys[(int)ConsoleKey.LeftArrow] = Key.LeftArrow;
+            _keys[(int)ConsoleKey.UpArrow] = Key.UpArrow;
+            _keys[(int)ConsoleKey.RightArrow] = Key.RightArrow;
+            _keys[(int)ConsoleKey.DownArrow] = Key.DownArrow;
+            _keys[(int)ConsoleKey.Insert] = Key.Insert;
+            _keys[(int)ConsoleKey.Delete] = Key.Delete;
+            _keys[(int)ConsoleKey.End] = Key.End;
+            _keys[(int)ConsoleKey.Home] = Key.Home;
+            _keys[(int)ConsoleKey.PageUp] = Key.PageUp;
+            _keys[(int)ConsoleKey.PageDown] = Key.PageDown;
+            _keys[(int)ConsoleKey.Backspace] = Key.Backspace;
+            _keys[(int)ConsoleKey.Tab] = Key.Tab;
+            _keys[(int)ConsoleKey.Spacebar] = Key.Spacebar;
+            _keys[(int)ConsoleKey.Enter] = Key.Enter;
+            _keys[(int)ConsoleKey.Escape] = Key.Escape;
+            _keys[(int)ConsoleKey.Sleep] = Key.Sleep;
+            _keys[(int)ConsoleKey.PrintScreen] = Key.PrintScreen;
+            _keys[(int)ConsoleKey.LeftWindows] = Key.LeftWindows;
+            _keys[(int)ConsoleKey.RightWindows] = Key.RightWindows;
+            _keys[(int)ConsoleKey.Applications] = Key.Applications;
+            _keys[(int)ConsoleKey.MediaNext] = Key.MediaNext;
+            _keys[(int)ConsoleKey.MediaPrevious] = Key.MediaPrevious;
+            _keys[(int)ConsoleKey.MediaStop] = Key.MediaStop;
+            _keys[(int)ConsoleKey.MediaPlay] = Key.MediaPlay;
+            _keys[(int)ConsoleKey.VolumeMute] = Key.VolumeMute;
+            _keys[(int)ConsoleKey.VolumeDown] = Key.VolumeDown;
+            _keys[(int)ConsoleKey.VolumeUp] = Key.VolumeUp;
+            _keys[(int)ConsoleKey.LaunchApp1] = Key.LaunchApp1;
+            _keys[(int)ConsoleKey.LaunchApp2] = Key.LaunchApp2;
+            _keys[(int)ConsoleKey.LaunchMail] = Key.LaunchMail;
+            _keys[(int)ConsoleKey.BrowserBack] = Key.BrowserBack;
+            _keys[(int)ConsoleKey.BrowserForward] = Key.BrowserForward;
+            _keys[(int)ConsoleKey.BrowserRefresh] = Key.BrowserRefresh;
+            _keys[(int)ConsoleKey.BrowserStop] = Key.BrowserStop;
+            _keys[(int)ConsoleKey.BrowserSearch] = Key.BrowserSearch;
+            _keys[(int)ConsoleKey.BrowserFavorites] = Key.BrowserFavorites;
+            _keys[(int)ConsoleKey.BrowserHome] = Key.BrowserHome;
+            _keys[(int)ConsoleKey.Oem1] = Key.OemSemicolon;
+            _keys[(int)ConsoleKey.Oem2] = Key.OemQuestion;
+            _keys[(int)ConsoleKey.Oem3] = Key.OemTilde;
+            _keys[(int)ConsoleKey.Oem4] = Key.OemOpenBrackets;
+            _keys[(int)ConsoleKey.Oem5] = Key.OemPipe;
+            _keys[(int)ConsoleKey.Oem6] = Key.OemCloseBrackets;
+            _keys[(int)ConsoleKey.Oem7] = Key.OemQuotes;
+            _keys[(int)ConsoleKey.Oem102] = Key.OemBackslash;
+            _keys[(int)ConsoleKey.OemPlus] = Key.OemPlus;
+            _keys[(int)ConsoleKey.OemComma] = Key.OemComma;
+            _keys[(int)ConsoleKey.OemMinus] = Key.OemMinus;
+            _keys[(int)ConsoleKey.OemPeriod] = Key.OemPeriod;
+
+            var CAlt = ConsoleModifiers.Alt;
+            var CShift = ConsoleModifiers.Shift;
+            var CControl = ConsoleModifiers.Control;
+
+            var None = KeyModifiers.None;
+            var Alt = KeyModifiers.Alt;
+            var Shift = KeyModifiers.Shift;
+            var Control = KeyModifiers.Control;
+
+            _modifiers = new KeyModifiers[8];
+
+            _modifiers[0] = None;
+            _modifiers[(int)CAlt] = Alt;
+            _modifiers[(int)CShift] = Shift;
+            _modifiers[(int)CControl] = Control;
+            _modifiers[(int)(CAlt | CShift)] = Alt | Shift;
+            _modifiers[(int)(CAlt | CControl)] = Alt | Control;
+            _modifiers[(int)(CShift | CControl)] = Shift | Control;
+            _modifiers[(int)(CAlt | CShift | CControl)] = Alt | Shift | Control;
         }
 
-        public Key ConvertKey(ConsoleKey key)
-        {
-            if (!_keys.TryGetValue(key, out var result))
-            {
-                result = Key.Unknown;
-            }
-            return result;
-        }
+        public Key ConvertKey(ConsoleKey key) => _keys[(int)key];
 
-        public KeyModifiers ConvertModifiers(ConsoleModifiers modifiers)
-        {
-            var result = KeyModifiers.None;
-
-            if (modifiers.HasFlag(ConsoleModifiers.Alt))
-            {
-                result |= KeyModifiers.Alt;
-            }
-            if (modifiers.HasFlag(ConsoleModifiers.Shift))
-            {
-                result |= KeyModifiers.Shift;
-            }
-            if (modifiers.HasFlag(ConsoleModifiers.Control))
-            {
-                result |= KeyModifiers.Control;
-            }
-
-            return result;
-        }
+        public KeyModifiers ConvertModifiers(ConsoleModifiers modifiers) => _modifiers[(int)modifiers];
     }
 }
