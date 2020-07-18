d85f655bb3d85a2e1940b4bdbdf095ff98d9ae96
Maxim Kornilov <sing1e7max@gmail.com>
2020-07-18 08:18:41 +0500

    Add VirtualKeyboard

---
+++ src/VoidMain.Common/IO/Keyboard/VirtualKeyboard.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace VoidMain.IO.Keyboard
+{
+    public class VirtualKeyboard : IKeyReader, IDisposable
+    {
+        private readonly Channel<KeyInput> _keysChannel;
+        private readonly ChannelReader<KeyInput> _reader;
+        private readonly ChannelWriter<KeyInput> _writer;
+        private KeyInput _nextKey;
+        private bool _hasNext;
+
+        public VirtualKeyboard()
+        {
+            var options = new UnboundedChannelOptions
+            {
+                AllowSynchronousContinuations = false,
+                SingleReader = true,
+                SingleWriter = true
+            };
+
+            _keysChannel = Channel.CreateUnbounded<KeyInput>(options);
+            _reader = _keysChannel.Reader;
+            _writer = _keysChannel.Writer;
+        }
+
+        public void Dispose() => _writer.TryComplete();
+
+        public async ValueTask<KeyInput> ReadKeyAsync(CancellationToken token = default)
+        {
+            if (!_hasNext)
+            {
+                _nextKey = await _reader.ReadAsync(token);
+            }
+
+            _hasNext = _reader.TryRead(out var nextKey);
+            var key = new KeyInput(_nextKey.KeyInfo, _nextKey.Character, _hasNext);
+            _nextKey = nextKey;
+            return key;
+        }
+
+        public bool TryWriteKey(KeyInput input) => _writer.TryWrite(input);
+    }
+}
