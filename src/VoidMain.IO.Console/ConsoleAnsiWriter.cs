22bdddaa201464b3eea7a7ce3a4dbea0544372c8
Maxim Kornilov <sing1e7max@gmail.com>
2020-07-18 07:48:51 +0500

    Extract ANSI escape sequence writer to a separate class

---
+++ src/VoidMain.IO.Console/ConsoleAnsiWriter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace VoidMain.IO.Console
+{
+    public class ConsoleAnsiWriter
+    {
+        private const string Reset = "\x1B[0m";
+        private const string DefaultForeground = "\x1B[39m";
+        private const string DefaultBackground = "\x1B[49m";
+
+        private readonly IConsole _console;
+        private readonly ReadOnlyMemory<char> _format;
+        private readonly Memory<char> _setColor;
+        private readonly Memory<char> _redSpan;
+        private readonly Memory<char> _greenSpan;
+        private readonly Memory<char> _blueSpan;
+
+        public ConsoleAnsiWriter(IConsole console)
+        {
+            _console = console ?? throw new ArgumentNullException(nameof(console));
+            _format = "D3".AsMemory();
+            _setColor = "\x1B[38;2;000;000;000m".ToCharArray();
+            _redSpan = _setColor.Slice(7, 3);
+            _greenSpan = _setColor.Slice(11, 3);
+            _blueSpan = _setColor.Slice(15, 3);
+        }
+
+        public void WriteForeground(Color color) => WriteColor(color, '3');
+
+        public void WriteBackground(Color color) => WriteColor(color, '4');
+
+        public void WriteDefaultForeground() => _console.Write(DefaultForeground);
+
+        public void WriteDefaultBackground() => _console.Write(DefaultBackground);
+
+        public void WriteReset() => _console.Write(Reset);
+
+        private void WriteColor(Color color, char code)
+        {
+            var span = _setColor.Span;
+            span[2] = code;
+
+            var format = _format.Span;
+            color.R.TryFormat(_redSpan.Span, out _, format);
+            color.G.TryFormat(_greenSpan.Span, out _, format);
+            color.B.TryFormat(_blueSpan.Span, out _, format);
+
+            _console.Write(span);
+        }
+    }
+}
--- src/VoidMain.IO.Console/ConsoleStyledTextWriter.cs
+++ src/VoidMain.IO.Console/ConsoleStyledTextWriter.cs
@@ -1,66 +1,38 @@
-﻿using System;
-using System.Runtime.CompilerServices;
-using VoidMain.Text;
+﻿using VoidMain.Text;
 
 namespace VoidMain.IO.Console
 {
     public class ConsoleStyledTextWriter : ConsoleTextWriter, IStyledTextWriter<TextStyle>
     {
-        private const string _resetStyle = "\x1B[0m";
-        private const string _defaultForeground = "\x1B[39m";
-        private const string _defaultBackground = "\x1B[49m";
-        private readonly ReadOnlyMemory<char> _format;
-        private readonly Memory<char> _setColor;
-        private readonly Memory<char> _redSpan;
-        private readonly Memory<char> _greenSpan;
-        private readonly Memory<char> _blueSpan;
+        private readonly ConsoleAnsiWriter _ansi;
 
         public ConsoleStyledTextWriter(IConsole console)
-            : base(console)
+                : base(console)
         {
-            _setColor = "\x1B[38;2;000;000;000m".ToCharArray();
-            _redSpan = _setColor.Slice(7, 3);
-            _greenSpan = _setColor.Slice(11, 3);
-            _blueSpan = _setColor.Slice(15, 3);
-            _format = "D3".AsMemory();
+            _ansi = new ConsoleAnsiWriter(console);
         }
 
-        public void ClearStyle() => _console.Write(_resetStyle);
+        public void ClearStyle() => _ansi.WriteReset();
 
         public void WriteStyle(TextStyle style)
         {
-            var span = _setColor.Span;
-
             if (style.Foreground is Color foreground)
             {
-                span[2] = '3'; // 38 is foreground
-                FormatColor(foreground);
-                _console.Write(span);
+                _ansi.WriteForeground(foreground);
             }
             else
             {
-                _console.Write(_defaultForeground);
+                _ansi.WriteDefaultForeground();
             }
 
             if (style.Background is Color background)
             {
-                span[2] = '4'; // 48 is background
-                FormatColor(background);
-                _console.Write(span);
+                _ansi.WriteBackground(background);
             }
             else
             {
-                _console.Write(_defaultBackground);
+                _ansi.WriteDefaultBackground();
             }
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private void FormatColor(Color color)
-        {
-            var format = _format.Span;
-            color.R.TryFormat(_redSpan.Span, out _, format);
-            color.G.TryFormat(_greenSpan.Span, out _, format);
-            color.B.TryFormat(_blueSpan.Span, out _, format);
         }
     }
 }
